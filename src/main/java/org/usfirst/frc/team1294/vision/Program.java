06834bdd8ecaa5f2518844f148222372bc0071fc
demilitarizedfork <michael8here@gmail.com>
2016-01-12 21:16:14 -0800

    added opencv dependency and grabbed single frame from webcam and saved it to disk.

--- src/main/java/org/usfirst/frc/team1294/vision/Program.java
+++ src/main/java/org/usfirst/frc/team1294/vision/Program.java
@@ -1,9 +1,22 @@
 package org.usfirst.frc.team1294.vision;
 
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.videoio.VideoCapture;
+
 public class Program {
 
+	static {
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+	}
 	public static void main(String[] args) {
 		System.out.println("Hello");
+		Mat originalImage = new Mat();
+		VideoCapture capture =new VideoCapture(0);
+		capture.read(originalImage);
+		
+		Imgcodecs.imwrite("test.jpg", originalImage);
 	}
 
 }
